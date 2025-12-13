3d33f551f0e2bb78d429a6f0748039420848050b
Nafees Haider <haidernafees161@gmail.com>
2025-12-13 17:01:25 +0500

    Fix header logo to use hosted image URL

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div className="flex items-center">
       <img
-        src="/logo.png"
+        src="https://iili.io/fY3vX6J.png"
         alt="Grocofy"
         className="h-8 w-auto object-contain"
       />
